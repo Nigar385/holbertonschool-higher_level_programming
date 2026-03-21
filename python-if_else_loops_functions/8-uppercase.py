52ce3191a61645c9bcba50c403bb742dcb5112d7
Nigar385 <13627@holbertonstudenst.com>
2026-03-21 15:18:50 +0000

    Implement uppercase function converting letters using ASCII

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,6 @@
 def uppercase(str):
     """Print a string in uppercase without using str.upper()."""
     for c in str:
-        #  ^yg ^yr ki  ik h ^yrfdirs ^y, b  y  k h ^yrf ^y   evir
         if ord('a') <= ord(c) <= ord('z'):
             print("{}".format(chr(ord(c) - 32)), end="")
         else:
