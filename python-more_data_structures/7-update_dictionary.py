51593546aa6eab2fd98a20bfa8245727f73cc489
Nigar385 <13627@holbertonstudents.com>
2026-03-28 15:58:59 +0000

    Add function to update or add key/value in dictionary

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    """Update or add key/value in dictionary."""
+    """Lüğətdə açarı yeniləyən və ya əlavə edən funksiya"""
     a_dictionary[key] = value
     return a_dictionary
