03b3a31389ff9179dc6f20983e07451f07e98b37
Nigar385 <13627@holbertonstudents.com>
2026-04-08 14:51:45 +0000

    Add inherits_from function to check subclass relationships

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""This module defines a function that checks if an object inherits from a class."""
+"""This module defines a function that checks inheritance."""
 
 
 def inherits_from(obj, a_class):
     """
-    Return True if obj is an instance of a subclass of a_class.
+    Return True if obj is a subclass instance of a_class.
     Otherwise return False.
     """
     return isinstance(obj, a_class) and type(obj) is not a_class
