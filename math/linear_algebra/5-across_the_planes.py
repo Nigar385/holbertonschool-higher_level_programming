504f8fb936415e82b977e9e419c189b8cae8e671
Nigar385 <13627@holbertonstudents.com>
2026-04-24 18:19:10 +0000

    message

---
+++ math/linear_algebra/5-across_the_planes.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Module that defines add_matrices2D function"""
+
+
+def add_matrices2D(mat1, mat2):
+    """Adds two 2D matrices element-wise"""
+    if len(mat1) != len(mat2):
+        return None
+    for i in range(len(mat1)):
+        if len(mat1[i]) != len(mat2[i]):
+            return None
+    return [[mat1[i][j] + mat2[i][j]
+             for j in range(len(mat1[i]))]
+            for i in range(len(mat1))]
