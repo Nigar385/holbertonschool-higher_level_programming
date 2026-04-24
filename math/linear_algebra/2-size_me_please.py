aa60acaef83e4d5cd485c59a790639ce87aa0235
Nigar385 <13627@holbertonstudents.com>
2026-04-24 18:12:33 +0000

    ratata

---
+++ math/linear_algebra/2-size_me_please.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Module that defines matrix_shape function"""
+
+
+def matrix_shape(matrix):
+    """Returns the shape of a matrix"""
+    shape = []
+    while isinstance(matrix, list):
+        shape.append(len(matrix))
+        matrix = matrix[0]
+    return shape
